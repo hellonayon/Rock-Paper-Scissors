b888546ab8ecb07f38f2b929fbddb1c029a1d5c1
BabyBear <85198585+hellonayon@users.noreply.github.com>
2022-07-14 11:10:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const userChoiceDisplay =  document.getElementById('user-choice')
 const resultDisplay =  document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click',(e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
@@ -23,4 +26,30 @@ function generateComputerChoice() {
         computerChoice = 'scissors'
     }
     computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+
+    if (computerChoice === userChoice ) {
+        result = 'its a draw!'
+    }
+    if (computerChoice === 'rock' && userChoice === "paper" ) {
+        result = 'you win!'
+    }
+    if (computerChoice === 'rock' && userChoice === "scissors" ) {
+        result = 'you lost!'
+    }
+    if (computerChoice === 'paper' && userChoice === "scissors" ) {
+        result = 'you win!'
+    }
+    if (computerChoice === 'paper' && userChoice === "rock" ) {
+        result = 'you lose!'
+    }
+    if (computerChoice === 'scissors' && userChoice === "rock" ) {
+        result = 'you win!'
+    }
+    if (computerChoice === 'scissors' && userChoice === "paper" ) {
+        result = 'you lost!'
+    }
+    resultDisplay.innerHTML = result
 }
